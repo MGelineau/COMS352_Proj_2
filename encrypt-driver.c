49370349a8f982d5b514504c2ba9e4e23c9b11e9
MGelineau <gelineau1234321@gmail.com>
2024-12-02 19:03:36 -0600

    Initial Prompts

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "encrypt-module.h"
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <string.h>
 
 void reset_requested() {
 	log_counts();
@@ -9,14 +13,29 @@ void reset_finished() {
 }
 
 int main(int argc, char *argv[]) {
-	init("in.txt", "out.txt", "log.txt"); 
-	char c;
-	while ((c = read_input()) != EOF) { 
-		count_input(c); 
-		c = encrypt(c); 
-		count_output(c); 
-		write_output(c); 
-	} 
+	    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <input_file> <output_file> <log_file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    // Initialize the system
+    init(argv[1], argv[2], argv[3]);
+
+    int N, M;
+    printf("Enter input buffer size (N > 1): ");
+    scanf("%d", &N);
+    printf("Enter output buffer size (M > 1): ");
+    scanf("%d", &M);
+
+    if (N <= 1 || M <= 1) {
+        fprintf(stderr, "Buffer sizes must be greater than 1.\n");
+        return EXIT_FAILURE;
+    }
+
+    init_buffer(&inputBuffer, N);
+    init_buffer(&outputBuffer, M);
+
+    pthread_t threads[5];
 	printf("End of file reached.\n"); 
 	log_counts();
 }
